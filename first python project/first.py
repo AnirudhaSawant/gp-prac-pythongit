b75710dda6ecee27f557bb9f174c0dd052f0c34b
anirudha <anirudhasawant02.as@gmail.com>
2024-06-17 12:06:05 +0530

    Added code for user input

--- first python project/first.py
+++ first python project/first.py
@@ -1,8 +1,14 @@
 def perimeter(length, breadth):
-	p = 2 * (length + breadth)
+	p = 3 * (length + breadth)
 	return p
     
 def area(length, breadth):
     a = length * breadth
     return a
     
+ l = input("Enter the Length: ")
+ b = input("Enter the Breadth: ")
+ 
+ print(perimeter(l,b))
+ print(area(l,b))
+ 
